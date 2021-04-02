a37cfd6641363aa1916188a0d5e81eb1ca770c5d
jinyoonc <58943909+jinyoonc@users.noreply.github.com>
2021-04-02 18:46:49 +0900

    Created sui-accordion and created sticky buttons on ss-card

--- src/plugins/skateui.js
+++ src/plugins/skateui.js
@@ -1,11 +1,13 @@
 import SuiButton from '../components/sui-button';
 import SuiCard from '../components/sui-card';
+import SuiAccordion from '../components/sui-accordion';
 import {ColorMangle} from "colormangle";
 
 export default {
     install(Vue, options) {
         Vue.component('sui-card', SuiCard);
         Vue.component('sui-button', SuiButton);
+        Vue.component('sui-accordion', SuiAccordion);
 
         let cs = new ColorMangle('#00807f').colorScheme();
         let body = document.getElementsByTagName('BODY')[0];
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -31,7 +31,7 @@
                     Content inside the card has an indent/styling depending on what/where you put in stuff
                 br
                 br
-                .sui-accordion(onclick="sui_accordion.handler(event)")
+                sui-accordion
                     .title Fonts
                     hr
                     .content(style="padding: 0 .5em;" onclick="(function(event){event.stopPropagation()})(event)")
@@ -157,9 +157,10 @@
                 h4 Look! A Button!
                 br
                 p We will use the ".button_footer" div for card buttons.
-            template(#buttonFooter).hideonphone // Jinyoon redo this part
+            template(#buttonFooter)
+                //.hideonphone // Jinyoon redo this part
                 sui-button HELLO
-            .button_footer.sticky.viewonphone
+            //.button_footer.sticky.viewonphone
                 sui-button HELLO
         br
         br
@@ -1224,42 +1225,42 @@ export default {
             }
         };
         window.sui_textarea.init();
-        window.sui_accordion = {
-            timeout: null,
-            handler: (ev) => {
-                let el = ev.target.closest('.sui-accordion');
-
-                if (!el)
-                    return;
-
-                let bool = !el.classList.contains('minus');
-
-                if (bool)
-                    el.classList.add('minus');
-                else
-                    el.classList.remove('minus');
-
-                if (el)
-                    el = el.lastChild;
-
-                if (window.sui_accordion.timeout)
-                    clearTimeout(window.sui_accordion.timeout);
-                window.sui_accordion.timeout = null;
-
-                if (bool) {
-                    el.style.maxHeight = '100vh';
-                    window.sui_accordion.timeout = setTimeout(() => {
-                        // use this.$nextTick() for vue
-                        el.style.maxHeight = 'unset';
-                    }, 750);
-                } else {
-                    el.style.maxHeight = '100vh';
-                    window.sui_accordion.timeout = setTimeout(() => {
-                        el.style.maxHeight = '0';
-                    }, 100);
-                }
-            }
-        };
+        // window.sui_accordion = {
+        //     timeout: null,
+        //     handler: (ev) => {
+        //         let el = ev.target.closest('.sui-accordion');
+        //
+        //         if (!el)
+        //             return;
+        //
+        //         let bool = !el.classList.contains('minus');
+        //
+        //         if (bool)
+        //             el.classList.add('minus');
+        //         else
+        //             el.classList.remove('minus');
+        //
+        //         if (el)
+        //             el = el.lastChild;
+        //
+        //         if (window.sui_accordion.timeout)
+        //             clearTimeout(window.sui_accordion.timeout);
+        //         window.sui_accordion.timeout = null;
+        //
+        //         if (bool) {
+        //             el.style.maxHeight = '100vh';
+        //             window.sui_accordion.timeout = setTimeout(() => {
+        //                 // use this.$nextTick() for vue
+        //                 el.style.maxHeight = 'unset';
+        //             }, 750);
+        //         } else {
+        //             el.style.maxHeight = '100vh';
+        //             window.sui_accordion.timeout = setTimeout(() => {
+        //                 el.style.maxHeight = '0';
+        //             }, 100);
+        //         }
+        //     }
+        // };
     }
 
 };
@@ -1742,62 +1743,6 @@ label.sui-radio {
         box-shadow: 0 0 0 2px var(--content-text_soft);
         border-radius: 1em;
         vertical-align: middle;
-    }
-}
-
-div.sui-accordion {
-    position: relative;
-
-    & > .title {
-        & + hr {
-            margin-top: 0;
-            border-left: 0;
-            border-right: 0;
-            max-width: unset;
-            width: 100%;
-        }
-
-        &::after {
-            content: "+";
-            font-size: 1.5em;
-            line-height: 2rem;
-            position: absolute;
-            right: 0;
-            font-weight: 300;
-            width: 1em;
-            text-align: center;
-        }
-
-        padding-right: 1.5em;
-        padding-left: .25rem;
-        box-sizing: border-box;
-        width: 100%;
-        cursor: pointer;
-        position: relative;
-        line-height: 2rem;
-
-        color: var(--content-text_soft);
-
-        &:hover {
-            text-shadow: 1px 1px var(--content-text_shadow);
-            color: var(--content-text);
-
-            &::after {
-                text-shadow: 1px 1px var(--content-text_shadow);
-            }
-        }
-    }
-
-    &.minus > .title {
-        &::after {
-            content: "-";
-        }
-    }
-
-    & > .content {
-        overflow: hidden;
-        max-height: 0;
-        transition: max-height .5s;
     }
 }
 
