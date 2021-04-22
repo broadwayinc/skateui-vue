3b6c69f952e606dc5da84ba309e8a719093dbd7f
Baksa Gimm <baksa@broadwayinc.com>
2021-04-23 08:59:30 +0900

    sui-image - now has grandparent div. sui-image can be styled directly from outside the vue component

--- src/components/sui-image.vue
+++ src/components/sui-image.vue
@@ -13,7 +13,7 @@ export default {
         onLoad: Function,
         onError: Function,
         nullImage: String,
-        brokenMsg: String,
+        brokenMsg: {type: String, default: ''},
         onClick: Function,
         onZoom: Function,
         src: Object | String,
@@ -46,7 +46,7 @@ export default {
     },
     watch: {
         src(n, o) {
-            console.log('image src watch...',n)
+            console.log('image src watch...', n);
             if (this.sui_image)
                 this.sui_image.setSrc(n, this.lightBox);
         }
@@ -57,118 +57,117 @@ export default {
 <style lang="less">
 @import "../assets/viewport";
 
-img {
-    color: transparent;
-
-    &:after {
-        text-align: center;
-        content: attr(alt);
-        font-size: 0.66rem;
-        color: var(--content-placeholder);
-        margin: auto;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        position: absolute;
-        z-index: 1;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        background-image: var(--null-image);
-        border: dashed 4px;
-        box-sizing: border-box;
-        border-color: var(--content-text_shadow);
-        background-color: var(--content);
-    }
-}
-
-.sui-image {
+.sui-image-parent {
     width: 100%;
-    position: relative;
-    overflow: hidden;
-    box-sizing: border-box;
-    display: inline-block;
 
-    & > img {
-        display: block;
+    .sui-image {
         width: 100%;
-        top: 0;
-
-        &.ratio {
-            position: absolute;
-            height: 100%;
-            object-fit: cover;
-        }
-    }
-
-
-    & > ._zoomer {
-        i {
-            font-size: 2rem;
-            color: rgba(0, 0, 0, 0.5);
-        }
-
-        background-color: rgba(255, 255, 255, 0.5);
-        border: 2px solid rgba(0, 0, 0, 0.25);
-        position: absolute;
-        z-index: 1;
-        pointer-events: none;
-        align-items: center;
-        justify-content: center;
+        position: relative;
         box-sizing: border-box;
+        display: inline-block;
 
-        * {
-            pointer-events: none;
-        }
-    }
-}
+        & > img {
+            display: block;
+            width: 100%;
+            top: 0;
+            color: transparent;
 
-.sui-image-lightBox {
-    display: none;
-    max-width: 100%;
-
-    padding: 8px;
-    @media @phone {
-        padding: 0;
-    }
-
-    & > img {
-        box-shadow: rgba(0,0,0,0.05) 0 0 48px;
-        display: block;
-        margin: auto;
-        max-height: 100%;
-        max-width: 100%;
-        min-width: 240px;
-        min-height: 240px;
-    }
-
-    & > ._loader {
-        display: block;
-
-        &._hideLoader {
-            display: none;
-        }
-
-        border: .15em solid var(--content-text_transparent);
-        border-radius: 50%;
-        border-top: .15em solid var(--button-text);
-        vertical-align: middle;
-
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin: -.5rem -.5rem;
-
-        width: 1rem;
-        height: 1rem;
-        animation: spin 2s linear infinite;
-        @keyframes spin {
-            0% {
-                transform: rotate(0deg);
+            &.ratio {
+                position: absolute;
+                height: 100%;
+                object-fit: cover;
             }
-            100% {
-                transform: rotate(360deg);
+
+            &:after {
+                text-align: center;
+                content: attr(alt);
+                font-size: 0.66rem;
+                color: var(--content-text_placeholder);
+                margin: auto;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                position: absolute;
+                z-index: 1;
+                top: 0;
+                left: 0;
+                width: 100%;
+                height: 100%;
+                background-image: var(--null-image);
+                border: dashed 4px;
+                box-sizing: border-box;
+                border-color: var(--content-text_shadow);
+                background-color: var(--content);
+            }
+        }
+
+        & > ._zoomer {
+            i {
+                font-size: 2rem;
+                color: rgba(0, 0, 0, 0.5);
+            }
+
+            background-color: rgba(255, 255, 255, 0.5);
+            border: 2px solid rgba(0, 0, 0, 0.25);
+            position: absolute;
+            z-index: 1;
+            pointer-events: none;
+            align-items: center;
+            justify-content: center;
+            box-sizing: border-box;
+
+            * {
+                pointer-events: none;
+            }
+        }
+    }
+
+    .sui-image-lightBox {
+        display: none;
+        max-width: 100%;
+
+        padding: 8px;
+        @media @phone {
+            padding: 0;
+        }
+
+        & > img {
+            box-shadow: rgba(0, 0, 0, 0.05) 0 0 48px;
+            display: block;
+            margin: auto;
+            max-height: 100%;
+            max-width: 100%;
+            min-width: 240px;
+            min-height: 240px;
+        }
+
+        & > ._loader {
+            display: block;
+
+            &._hideLoader {
+                display: none;
+            }
+
+            border: .15em solid var(--content-text_transparent);
+            border-radius: 50%;
+            border-top: .15em solid var(--button-text);
+            vertical-align: middle;
+
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            margin: -.5rem -.5rem;
+
+            width: 1rem;
+            height: 1rem;
+            animation: spin 2s linear infinite;
+            @keyframes spin {
+                0% {
+                    transform: rotate(0deg);
+                }
+                100% {
+                    transform: rotate(360deg);
+                }
             }
         }
     }
