a663e4e470d87b1c70e1ddb19d9bca7980361f45
Baksa Gimm <baksa@broadwayinc.com>
2021-04-23 09:26:04 +0900

    sui-app, sui-sticky moved to plugin loader file skateui.js

--- src/main.js
+++ src/main.js
@@ -3,16 +3,11 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
-import sui_app from './components/sui-app'
-import sui_sticky from './components/sui-sticky'
 import Skateui from "./plugins/skateui.js"
 
 Vue.use(Skateui);
 
 Vue.config.productionTip = false
-
-Vue.component('sui-app',sui_app);
-Vue.component('sui-sticky',sui_sticky);
 
 new Vue({
   router,
