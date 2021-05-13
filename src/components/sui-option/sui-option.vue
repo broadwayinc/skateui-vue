44a034fa89bc9fb1bad46b6f3f8fb7c1f2691408
dev <dev@broadwayinc.com>
2021-05-13 05:01:54 +0000

    updated to version 0.1.35

--- src/components/sui-option/sui-option.vue
+++ src/components/sui-option/sui-option.vue
@@ -1,3 +1,3 @@
 <template lang="pug" src="sui-option.pug"></template>
 <script src="sui-option.js"></script>
-<style lang="less" scoped="" src="sui-option.less"></style>
+<style lang="less" scoped src="sui-option.less"></style>
