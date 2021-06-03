a689061de57a6de3400294f826db1c549a374e74
jinyoonc <58943909+jinyoonc@users.noreply.github.com>
2021-06-03 10:19:33 +0900

    Comment removal

--- src/components/sui-step.vue
+++ src/components/sui-step.vue
@@ -27,41 +27,16 @@ export default {
             this.viewport = window.sui_app.viewport
         });
     },
-    updated() {
-      console.log("UIpdating")
-    },
     computed: {
         positionClass() {
             this.allowance = this.viewport === 'laptop' || this.viewport === 'desktop' ? 3 : 2;
             if (this.idx === this.complete) {
                 return 'active';
             }
-            // let left = this.complete - this.allowance;
-            //I was in the midst of calculating right
-            // let right = this.stepSize - this.complete;
-            // console.log({right})
-            // if (left < 0) {
-            //     right += (-1 * left);
-            //     left = this.allowance - left * -1;
-            // }
-            // if(left >= 0 && this.idx < this.complete) {
-            //     for(let i = this.complete; i >= this.complete - this.allowance; i--) {
-            //         if(this.idx === i) {
-            //             return "visible"
-            //         }
-            //     }
-            // }
-            //
-            // if(left >= 0 && this.idx < this.complete) {
-            //     for(let i = this.complete; i >= this.complete - this.allowance; i--) {
-            //         if(this.idx === i) {
-            //             return "visible"
-            //         }
-            //     }
-            // }
+
             let right = this.stepSize - this.complete;
             let left = this.allowance - right;
-            console.log(this.complete, this.allowance, left, right)
+
             if(left > 0) {
                 for(let i = this.complete; i > left; i--) {
                     if(this.idx === i) {
