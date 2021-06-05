925f4f6ea4722f1a7a14416f40714081e540fbb2
jinyoonc <58943909+jinyoonc@users.noreply.github.com>
2021-06-05 18:17:07 +0900

    Add missing validations for textarea as well as disabled.

--- src/components/sui-textarea.vue
+++ src/components/sui-textarea.vue
@@ -1,10 +1,10 @@
 <template lang='pug'>
-sui-label(type="textarea" :suffix='suffix' :prefix='prefix' :label="label" :error="error" :required="required" :message="message" :disabled="disabled")
+sui-label(type="textarea" :suffix='suffix' :prefix='prefix' :label="label" :error="isError" :required="required" :message="helperMessage" :disabled="disabled")
     template(#button-left)
         slot(name="button-left")
     template(#button-right)
         slot(name="button-right")
-    textarea(:placeholder="placeholder" rows="1" @keyup="(e)=>{keyOutput(e.code)}" )
+    textarea(ref="input" @input="updateValue()" @invalid.prevent="invalidInput" :placeholder="placeholder" rows="1" @keyup="(e)=>{keyOutput(e.code)}" :disabled="disabled" :required="required")
 </template>
 <script>
 export default {
@@ -15,13 +15,55 @@ export default {
         prefix: String,
         suffix: String,
         label: String,
-        required: Boolean,
+        required: [String, Boolean],
         disabled: Boolean,
         message: String,
+        value: {
+            type: [String, Number],
+            default: ''
+        },
         keyOutput: {
             type: Function,
             default: () => {
             }
+        }
+    },
+    data() {
+      return {
+          isTouched: false
+      }
+    },
+    computed: {
+        isError() {
+            return this.isInvalid || this.error;
+        },
+        helperMessage() {
+            let helper = this.message || null;
+            if (this.requireFail && this.isInvalid) {
+                if (typeof this.required === 'string') {
+                    helper = this.required;
+                }
+            } else if (typeof this.error === 'string') {
+                helper = this.error;
+            }
+            return helper;
+        },
+        isInvalid() {
+            return this.isTouched && this.requireFail;
+        },
+        requireFail() {
+            return this.required && this.value === '';
+        },
+    },
+    methods: {
+        updateValue(value) {
+            this.$emit('input', value ? value : this.$refs.input.value);
+        },
+        invalidInput() {
+            this.isTouched = true;
+            if (this.requireFail) {
+                this.$emit('requiredError');
+            } else this.$emit('error');
         }
     }
 };
