96d52ec982e2eb5db3de5f2b32b65ac66bfdbf65
baksagimm <baksa@broadwayinc.com>
2021-07-12 00:15:27 +0900

    Added license text in all component file

--- src/components/split-files.py
+++ src/components/split-files.py
@@ -9,6 +9,32 @@
 import os
 from bs4 import BeautifulSoup
 
+license_text = """
+/*
+MIT License
+
+Copyright (c) 2021 BROADWAY INC.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+"""
+
 for filename in os.listdir('./'):
     if filename.endswith(".vue") and filename[0] != '_':
 
@@ -38,7 +64,12 @@ for filename in os.listdir('./'):
 
                     if found:
                         with open("./{}/{}.{}".format(name[0], name[0], nav[tag]), "w", encoding='UTF8') as d:
-                            text = ''.join(found)
+
+                            prefix = ''
+                            if tag == 'script':
+                                prefix = license_text
+
+                            text = prefix + ''.join(found)
                             d.write(text.replace("../", "../../"))
 
                         found['src'] = name[0] + '.' + nav[tag]
