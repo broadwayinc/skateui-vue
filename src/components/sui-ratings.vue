0151d590df5013c2db7553a2aa08504b546cb864
seejwy <ba3466aca>
2021-11-03 11:07:33 +0900

    Created new Card and margin rules

---
+++ src/components/sui-ratings.vue
@@ -0,0 +1,23 @@
+<template functional lang='pug'>
+div
+    i.rating.material-icons(v-for="i in parseInt(props.rating)") star
+    i.rating.material-icons(v-if="props.rating % 1 >= 0.5") star_half
+</template>
+
+<script>
+export default {
+    name: 'sui-ratings',
+    props: {
+        rating: Number,
+    }
+};
+</script>
+<style lang="less" scoped>
+@import '../assets/viewport.less';
+i {
+    &.rating.material-icons {
+        font-size: 1rem;
+        color: #fed43b;
+    }
+}
+</style>
